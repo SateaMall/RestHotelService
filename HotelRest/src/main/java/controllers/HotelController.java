05fd4cf12540dab336fa0738d1081ade707a264d
Gürgün Dayıoğlu <gurgun.dayioglu@icloud.com>
2023-12-17 20:32:38 +0100

    Update HotelController.java

--- HotelRest/src/main/java/controllers/HotelController.java
+++ HotelRest/src/main/java/controllers/HotelController.java
@@ -103,7 +103,7 @@ public class HotelController {
 
     /***********	METHODES  ***********/
     public boolean signIn(int id, String mdp) {
-        hotel= Hrepository.getReferenceById(1L);
+        hotel= Hrepository.getReferenceById(0L);
         List<Agence> agences= hotel.getAgences();
         for(Agence agence: agences) {
             if (agence.getId() == id) {
